0529e63324c0c8a38434a4cdf57c31d9dc638a5e
radezmarija <60050821+radezmarija@users.noreply.github.com>
2021-03-24 12:05:52 +0100

    Update RunCucumberTest.java

--- IWECProject/src/test/java/testRunner/RunCucumberTest.java
+++ IWECProject/src/test/java/testRunner/RunCucumberTest.java
@@ -6,11 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Features/" , glue= "stepDefinition", plugin = { "html:target/cucumber-html-report",
-        "json:target/cucumber.json", 
-        "pretty:target/cucumber-pretty.txt",
-        "usage:target/cucumber-usage.json", 
-        "junit:target/cucumber-results.xml"}
+@CucumberOptions(features = "Features/" , glue= "stepDefinition", plugin = {"pretty"}
 )
 
 public class RunCucumberTest{
